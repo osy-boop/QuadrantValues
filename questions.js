13f83f0128688c1e7b56f974ae72755d6e77c35c
osy <66807433+osy-boop@users.noreply.github.com>
2021-10-15 14:45:16 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,6 +7,15 @@ questions = [
             "govt": -5,
             "scty": 0
         }
+    },    
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
     },
     {
         "question": "It is necessary for the government to intervene in the economy to protect consumers.",
